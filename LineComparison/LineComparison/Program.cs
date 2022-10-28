f7e26a0a05bb28b3d2dd42f613c7f6b3a5d3e028
Afzal Haroon Mohammed <afzalharoon.mohammed@bpktechmail.com>
2022-10-28 08:34:16 +0530

    [afzal]: Added Basic Folder Structure

---
+++ LineComparison/LineComparison/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineComparison
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Problem Statement");
+        }
+    }
+}
