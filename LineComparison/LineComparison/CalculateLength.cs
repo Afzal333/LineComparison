4cf7b650bfdf1dfa2e94a7e70da46525a94707ea
Afzal Haroon Mohammed <afzalharoon.mohammed@bpktechmail.com>
2022-10-28 08:50:35 +0530

    [Afzal] refactor: Implemented Line Camparison

--- LineComparison/LineComparison/CalculateLength.cs
+++ LineComparison/LineComparison/CalculateLength.cs
@@ -16,10 +16,11 @@ namespace LineComparison
             this.y1 = y1;
             this.y2 = y2;
         }
-        public void Calculate()
+        public double Calculate()
         {
             double length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
-            Console.WriteLine("Length:-" + length);            
+            Console.WriteLine("Length:-" + length);
+            return length;
         }
     }
 }
--- LineComparison/LineComparison/Program.cs
+++ LineComparison/LineComparison/Program.cs
@@ -7,8 +7,15 @@ namespace LineComparison
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Problem Statement");
-            CalculateLength calculateLength = new CalculateLength(5, 2, 5, 1);
-            calculateLength.Calculate();
+            CalculateLength calculateLength1 = new CalculateLength(5, 2, 5, 1);
+            double lenght1 = calculateLength1.Calculate();
+            CalculateLength calculateLength2 = new CalculateLength(5, 2, 5, 1);
+            double lenght2 = calculateLength2.Calculate();
+
+            if (lenght1.Equals(lenght2))
+                Console.WriteLine("Both lines are equal");
+            else
+                Console.WriteLine("Both Lines are not equal");            
         }
     }
 }
